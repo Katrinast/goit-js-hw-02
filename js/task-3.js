9fb7b6f833b787f727a9ae3d857f0a4d49c26275
Katrinast <stehnovicekaterina@gmail.com>
2024-03-13 19:11:09 +0200

    practice

--- js/task-3.js
+++ js/task-3.js
@@ -21,3 +21,73 @@
 // console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
 // console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
 
+// const salaries = {
+//   Jhon: 100,
+//   Ann: 160,
+//   Peter: 130,
+// };
+// const salariesArray = Object.values(salaries)
+
+// let sum = 0;
+// if (salariesArray.length) {
+//   for (const salary of salariesArray) {
+// sum += salary
+//   }
+//   console.log(sum);
+// } else {
+//   console.log(sum);
+// }
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+
+// const keys = [];
+// const values = [];
+// for (const key in apartment) {
+//   keys.push(key);
+//   values.push(apartment[key]);
+// }
+// console.log(keys);
+// console.log(values);
+
+// const book = {
+//   title: "The Last Kingdom",
+//   автор: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+// console.log(keys); // ['title', 'author', 'genres', 'rating']
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+
+// const values = [];
+// const keys = Object.keys(apartment);
+// console.log(keys);
+// for (const key of keys) {
+//   values.push(apartment[key]);
+  
+// }
+
+// console.log(values);
+
+function countTotalSalary(salaries) {
+  let totalSalary = 0;
+  const values = Object.values(salaries);
+  for (const salary of values){
+    totalSalary += salary
+  }
+  return totalSalary;
+
+}
+
+console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }));
+
+
